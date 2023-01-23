6eecaf2e967daced995ffd57912b85c1be42ce09
sergeyko2 <sergeyko2@gmail.com>
2023-01-23 04:30:53 +0200

    Add file

--- src/main/java/hw8/FileLoggerConfiguration.java
+++ src/main/java/hw8/FileLoggerConfiguration.java
@@ -2,15 +2,24 @@ package hw8;
 
 public class FileLoggerConfiguration {
 
-    private String fileName;
-    private LoggingLevel currentLoggingLevel;
-    private double maxSizeByte;
-    private String formatWrite;
+    private String fileName = "src\\main\\resources\\hw8\\log.txt";
+    private LoggingLevel currentLoggingLevel = LoggingLevel.DEBUG;
+    private double maxSizeByte = 500;
+    private String formatWrite = "[ПОТОЧНИЙ_ЧАС][DEBUG] Повідомлення: [СТРОКА-ПОВІДОМЛЕННЯ]";
 
-    public FileLoggerConfiguration(String fileName, LoggingLevel currentLoggingLevel, double maxSizeByte, String formatWrite) {
-        this.fileName = fileName;
-        this.currentLoggingLevel = currentLoggingLevel;
-        this.maxSizeByte = maxSizeByte;
-        this.formatWrite = formatWrite; // [ПОТОЧНИЙ_ЧАС][DEBUG] Повідомлення: [СТРОКА-ПОВІДОМЛЕННЯ]
+    public String getFileName() {
+        return fileName;
+    }
+
+    public LoggingLevel getCurrentLoggingLevel() {
+        return currentLoggingLevel;
+    }
+
+    public double getMaxSizeByte() {
+        return maxSizeByte;
+    }
+
+    public String getFormatWrite() {
+        return formatWrite;
     }
 }
