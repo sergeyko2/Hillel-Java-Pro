8f99581d0098ddfd2ec61f017451417019894a26
sergey <sergeyko2@gmail.com>
2023-02-15 16:03:03 +0200

    Final commit after review

--- src/main/java/hw9/task1/MainApp.java
+++ src/main/java/hw9/task1/MainApp.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class MainApp {
 
-    private static Random randomInt = new Random();
+    private static final Random randomInt = new Random();
 
     public static void main(String[] args) {
 
@@ -25,20 +25,19 @@ public class MainApp {
     private static String calcOccurance(List<String> stringList) {
         HashMap<String, Integer> wordCount = new HashMap<>();
         StringBuilder result = new StringBuilder();
-        for (String tempWord : stringList) {
-            if (wordCount.containsKey(tempWord)) {
-                wordCount.put(tempWord, wordCount.get(tempWord) + 1);
-            } else wordCount.put(tempWord, 1);
-        }
-        for (Map.Entry<String, Integer> entry : wordCount.entrySet())
+        stringList.forEach(word -> wordCount.merge(word, 1, Integer::sum));
+        for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
             result.append("\n\t").append(entry.getKey()).append(": ").append(entry.getValue());
+        }
         return result.toString();
     }
 
     private static List<Integer> findUnique(List<Integer> myList) {
         List<Integer> resultList = new ArrayList<>();
         for (int number : myList) {
-            if (!resultList.contains(number)) resultList.add(number);
+            if (!resultList.contains(number)) {
+                resultList.add(number);
+            }
         }
         return resultList;
     }
