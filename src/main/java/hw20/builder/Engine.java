19d39a75891194acdd3a2885aff1dd3c4b6e6697
sergey <sergeyko2@gmail.com>
2023-02-28 18:13:26 +0200

    final commit hw20 builder

---
+++ src/main/java/hw20/builder/Engine.java
@@ -0,0 +1,9 @@
+package hw20.builder;
+
+public class Engine {
+
+    @Override
+    public String toString() {
+        return "Petrol Engine ";
+    }
+}
---
+++ src/main/java/hw20/builder/Transmission.java
@@ -0,0 +1,8 @@
+package hw20.builder;
+
+public class Transmission {
+    @Override
+    public String toString() {
+        return "Manual Transmission";
+    }
+}
