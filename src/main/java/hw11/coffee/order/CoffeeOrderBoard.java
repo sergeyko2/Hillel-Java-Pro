9edc5935de1796bc1022ccc3951b4197e6a00710
sergey <sergeyko2@gmail.com>
2023-03-07 18:32:37 +0200

    final commit hw11

---
+++ src/main/java/hw11/coffee/order/CoffeeOrderBoard.java
@@ -0,0 +1,43 @@
+package hw11.coffee.order;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CoffeeOrderBoard {
+    private List<Order> orderList = new ArrayList<>();
+
+    public void deliver() {
+        if (orderList.isEmpty()) {
+            System.out.println("No order!");
+            return;
+        }
+        Order currentOrder = orderList.get(0);
+        System.out.printf("Order #%d for %s is now issued",
+                currentOrder.getNumber(), currentOrder.getClient());
+        System.out.println();
+        orderList.remove(0);
+    }
+
+    public void deliver(int number) {
+        for (Order order : orderList) {
+            if (order.getNumber() == number) {
+                System.out.printf("Order #%d for %s is now issued",
+                        order.getNumber(), order.getClient());
+                System.out.println();
+                orderList.remove(order);
+                return;
+            }
+        }
+    }
+
+    public void add(Order order) {
+        order.setNumber(orderList.size() + 1);
+        orderList.add(order);
+    }
+
+    public void draw() {
+        for (Order order : orderList) {
+            System.out.println(order);
+        }
+    }
+}
