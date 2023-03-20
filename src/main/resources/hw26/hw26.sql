f0546f665065239985e1c73fd25869abb681cff6
sergeyko2 <sergeyko2@gmail.com>
2023-03-20 18:43:29 +0200

    Initial commit hw22

--- src/main/resources/hw26/hw26.sql
+++ src/main/resources/hw26/hw26.sql
@@ -1,2 +1,24 @@
 SELECT * FROM offices o
 where o.officeCode=4 or o.officeCode=5 or o.officeCode=6 or o.officeCode=7;
+
+SELECT * FROM offices o
+where o.officeCode IN (4,5,6,7);
+
+SELECT * FROM offices o
+where o.officeCode between 4 AND 7;
+
+
+SELECT p.productName FROM products p
+WHERE p.productName LIKE '1936%';
+
+SELECT * FROM products p
+WHERE (p.productVendor='Motor City Art Classics' OR p.productVendor='Exoto Designs') AND p.productScale='1:24';
+
+SELECT * FROM orders o
+WHERE o.shippedDate IS NULL;
+
+SELECT * FROM offices o
+ORDER BY o.city DESC;
+
+SELECT distinct p.productVendor FROM products p
+ORDER BY p.productVendor ASC; 
