73463a50121ee4b27955b975f3ae765cac6206ba
sergey <sergeyko2@gmail.com>
2023-05-17 16:32:28 +0300

    Initial commit hw13

---
+++ src/main/java/hw13/MainApp.java
@@ -0,0 +1,9 @@
+package hw13;
+
+public class MainApp {
+    public static void main(String[] args) throws InterruptedException {
+        ValueCalculator valueCalculator = new ValueCalculator(15_000_000);
+        valueCalculator.calculateMethod();
+    }
+
+}
